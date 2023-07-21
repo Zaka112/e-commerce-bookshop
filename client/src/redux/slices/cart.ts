fa343328d4e0b90dd580d3b7a2d076864c13246c
Muhammad Zaka Ullah <97341177+Zaka112@users.noreply.github.com>
2023-07-21 11:50:10 +0200

    client cart-slice/store

---
+++ client/src/redux/slices/cart.ts
@@ -0,0 +1,63 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+import { CartBook, Book } from "../../types/types";
+
+type CartList = {
+  cartItems: CartBook[];
+};
+
+export const initialState: CartList = {
+  cartItems: [],
+};
+
+const cartSlice = createSlice({
+  name: "cartList",
+  initialState,
+  reducers: {
+    addToCart: (state, action: PayloadAction<Book>): void => {
+      const isIncluded = state.cartItems.some(
+        (cartItem) => cartItem._id === action.payload._id
+      );
+      if (!isIncluded) {
+        state.cartItems.push({ ...action.payload, counter: 1 });
+      }
+      else
+     {alert("already in") } // Currently this logic would not be executed as have check in component. Will look for some alert thing later.
+    },
+    removeFromCart: (state, action: PayloadAction<Book>) => {
+      const arrayAfterRemoving = state.cartItems.filter(
+        (item) => item._id !== action.payload._id
+      );
+      state.cartItems = arrayAfterRemoving;
+    },
+
+    increaseQuantity: (state, action: PayloadAction<CartBook>) => {
+      const itemIndex = state.cartItems.findIndex(
+        (cartItem) => cartItem._id === action.payload._id
+      );
+
+      if (itemIndex !== -1) {
+        state.cartItems[itemIndex].counter++;
+      }
+    },
+    decreaseQuantity: (state, action: PayloadAction<CartBook>) => {
+      const itemIndex= state.cartItems.findIndex(
+        (cartItem) => cartItem._id === action.payload._id
+      );
+      if (itemIndex !== -1) {
+        if (state.cartItems[itemIndex].counter === 1) {
+          state.cartItems[itemIndex].counter = 1;
+        } else {
+          state.cartItems[itemIndex].counter--;
+        }
+      }
+    },
+    checkOut: (state) => {
+      state.cartItems = initialState.cartItems;
+    },
+  },
+});
+
+export const cartListActions = cartSlice.actions;
+const cartReducer = cartSlice.reducer;
+export default cartReducer;
--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -2,11 +2,17 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import bookReducer from "./slices/books";
 import bookDetailReducer from "./slices/bookDetails";
+import searchReducer from "./slices/search";
+import userReducer from "./slices/user";
+import cartReducer from "./slices/cart";
 
 export const store = configureStore({
   reducer: {
     books: bookReducer,
     bookDetail: bookDetailReducer,
+    cartList:cartReducer,
+    search:searchReducer,
+    users:userReducer,
   },
 });
 

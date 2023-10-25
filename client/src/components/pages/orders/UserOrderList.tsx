38b09e3e5629adaaa4ca77bfe6845336c3419ecb
Muhammad Zaka Ullah <97341177+Zaka112@users.noreply.github.com>
2023-10-25 23:54:21 +0200

    orderlist

--- client/src/components/pages/orders/UserOrderList.tsx
+++ client/src/components/pages/orders/UserOrderList.tsx
@@ -20,9 +20,9 @@ import { AppDispatch, RootState } from "../../../redux/store";
 import { getUserOrderList } from "../../../redux/thunk/orders";
 import UserOrderItems from "./UserOrderItems";
 import NotFound from "../../Error";
+import UserList from "../../users/userRoleList/UserList";
 
 export default function UserOrdersList() {
-
   const [page, setPage] = useState<number>(0);
   const [rowsPerPage, setRowsPerPage] = useState<number>(10);
   const handleChangePage = (event: unknown, newPage: number) => {
@@ -33,7 +33,7 @@ export default function UserOrdersList() {
     event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => {
     setRowsPerPage(parseInt(event.target.value));
-   setPage(0);
+    setPage(0);
   };
 
   const navigate = useNavigate();
@@ -49,8 +49,8 @@ export default function UserOrdersList() {
       dispatch(getUserOrderList(userId));
     }
   }, [dispatch, userId, userDetails]);
-  if (userDetails) {
-    return (
+  if (userDetails ) {
+    if (orderList.length>0) {return (
       <Paper>
         <Typography variant="h4" component="div">
           OrderList{" "}
@@ -60,6 +60,7 @@ export default function UserOrdersList() {
           <Button onClick={() => navigate(-1)}>Back</Button>{" "}
           {userDetails.firstName + ` - your order history`}{" "}
         </Paper>
+        
         <TableContainer>
           <Table>
             <TableHead>
@@ -94,29 +95,31 @@ export default function UserOrdersList() {
               </TableRow>
             </TableHead>
 
-            {orderList.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((order) => (
-              <TableBody key={order._id}>
-                <UserOrderItems order={order}  />
-              </TableBody>
-
-            ))}
-            
+            {orderList
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((order) => (
+                <TableBody key={order._id}>
+                  <UserOrderItems order={order} />
+                </TableBody>
+              ))}
           </Table>
-         
         </TableContainer>
-        <TablePagination
-         ActionsComponent={TablePaginationActions}
-          rowsPerPage={rowsPerPage}
-          page={page}
-          count={orderList.length}
-          onPageChange={handleChangePage}
-          onRowsPerPageChange={handleChangeRowsPerPage}
-          component="div"
-        />
+        {orderList.length > 10 ? (
+          <TablePagination
+            ActionsComponent={TablePaginationActions}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            count={orderList.length}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+            component="div"
+          />
+        ) : null}
       </Paper>
-    );
+    )} 
+    else 
+    return <Typography variant="h4">You have not made any order yet</Typography> 
   } else {
     return <NotFound />;
   }
- 
 }

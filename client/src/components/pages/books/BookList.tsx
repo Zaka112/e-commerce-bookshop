a619ab8f16b8febfb88e9cc14b20b6b452beccde
Muhammad Zaka Ullah <97341177+Zaka112@users.noreply.github.com>
2024-01-17 22:18:01 +0100

    TODO/On sale

--- client/src/components/pages/books/BookList.tsx
+++ client/src/components/pages/books/BookList.tsx
@@ -2,25 +2,31 @@ import React, { useEffect } from "react";
 
 import { useDispatch, useSelector } from "react-redux";
 import {
+  Box,
   CircularProgress,
   Grid,
   Pagination,
   Paper,
   Typography,
 } from "@mui/material";
+import { Carousel } from "react-responsive-carousel";
 
 import { AppDispatch, RootState } from "redux/store";
 import { getBookList } from "redux/thunk/books";
 import BookItems from "./BookItems";
 import SearchForm from "../../search/SerachForm";
 import SortForm from "../../sort/SortForm";
+import { Col, Row } from "reactstrap";
+import { CenterFocusStrong } from "@mui/icons-material";
 
 export default function BookList() {
- // const [progress, setProgress] = React.useState(10);
+  // const [progress, setProgress] = React.useState(10);
   const [currentPage, setCurrentPage] = React.useState(1);
 
   const dispatch = useDispatch<AppDispatch>();
   const bookList = useSelector((state: RootState) => state.books.books);
+  //filter on sale
+  //const onSale= bookList.filter((books)=> books.onSale)
   const itemsPerPage = 5;
   const totalPages = Math.ceil(bookList.length / itemsPerPage);
   let isLoading = useSelector((state: RootState) => state.books.isLoading);
@@ -104,11 +110,36 @@ export default function BookList() {
         </Typography>
         <SearchForm />
         <SortForm />
+        {/* <Row>
+          {" "}
+          <Col md="2">
+            <Box sx={{ width: 300 }}>
+              <Carousel showThumbs={false} showStatus={false} autoPlay={true}>
+                {onSale.map((item) => {
+                  return (
+                    <img
+                      width={75}
+                      height={150}
+                      key={item._id}
+                      alt={item.title}
+                      src={
+                        item.images[
+                          Math.floor(Math.random() * item.images.length)
+                        ]
+                      }
+                    />
+                  );
+                })}
+              </Carousel>
+            </Box>
+          </Col>
+        </Row> */}
+
         <Grid
           container
           sx={{
             display: "grid",
-            gridTemplateColumns: "repeat(2, 1fr)",
+            gridTemplateColumns: "repeat(3, 1fr)",
             gap: "1rem",
           }}
         >
